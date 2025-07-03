890dced7dded972bc70e67631a23717525cb2907
22120419 <nguyenvanty147258@gmail.com>
2025-07-03 22:43:02 +0700

    update VoiceGenerator

--- src/services/audio.js
+++ src/services/audio.js
@@ -15,3 +15,17 @@ export const uploadAudio = async (audioData) => {
     throw error;
   }
 };
+
+export const createAudio = async (script, voice) => {
+  try {
+    console.log("script >>: ", script);
+    console.log("voice:>>", voice);
+    const response = await axiosInstance.post("/audio/generate", {
+      text: script,
+      voice: voice,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
